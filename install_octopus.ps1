d502a57f5760eaee381f992fa5a4926183285eb5
Sarasa Gunawardhana <SarasaGunawardhana@users.noreply.github.com>
2025-04-08 10:26:26 +0530

    Create install_octopus.ps1

---
+++ install_octopus.ps1
@@ -0,0 +1,14 @@
+$url = "https://download.octopusdeploy.com/octopus/Octopus.2021.3.12155-x64.msi"
+# $url = "https://octopus.com/downloads/OctopusServer/thanks?releaseVersion=2025.1.10012&filePlatform=WindowsX64"
+
+$outpath = "$PSScriptRoot\octopusdeploy.msi"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $url -OutFile $outpath
+
+$octoargs = @("/I",$outpath,"/quiet", "RUNMANAGERONEXIT=no")
+Write-Output $args[0]
+Write-Output $args[1]
+Write-Output $args[2]
+Write-Output $args[3]
+
+Start-Process "msiexec.exe" -ArgumentList $octoargs -Wait -NoNewWindow
