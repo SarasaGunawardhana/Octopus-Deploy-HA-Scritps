bd3c5ea25b42c283522f431519c453d867d2b49a
Sarasa Gunawardhana <SarasaGunawardhana@users.noreply.github.com>
2025-04-09 11:00:06 +0530

    Update install_octopus.ps1

--- install_octopus.ps1
+++ install_octopus.ps1
@@ -1,5 +1,5 @@
-$url = "https://download.octopusdeploy.com/octopus/Octopus.2021.3.12155-x64.msi"
-# $url = "https://octopus.com/downloads/OctopusServer/thanks?releaseVersion=2025.1.10012&filePlatform=WindowsX64"
+# $url = "https://download.octopusdeploy.com/octopus/Octopus.2021.3.12155-x64.msi"
+$url = "https://octopus.com/downloads/OctopusServer/thanks?releaseVersion=2025.1.10012&filePlatform=WindowsX64"
 
 $outpath = "$PSScriptRoot\octopusdeploy.msi"
 $ProgressPreference = 'SilentlyContinue'
